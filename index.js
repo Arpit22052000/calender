02bf99ea9a7bfdf206b5733994124c5137b45da0
Arpit22052000 <a.kabra470@gmail.com>
2023-12-13 16:29:06 +0530

    js-file

---
+++ index.js
@@ -0,0 +1,40 @@
+const currentDate = new Date();
+const fullDate = currentDate.toDateString();
+
+
+
+
+// const type = document.querySelector(".dateType")
+// type.addEventListener("click", () => {
+//     dayFormat()
+// })
+
+
+// function dayFormat() {
+//     alert("Choose the day format :")
+//     const fullDate = currentDate.toDateString();
+//     const localeDate = currentDate.toLocaleDateString();
+//     const localeTime = currentDate.toLocaleTimeString();
+//     const localeString = currentDate.toLocaleString();
+
+// }
+
+
+
+
+
+
+console.log(fullDate)
+console.log(localeDate)
+console.log(localeTime)
+console.log(localeString)
+
+
+const todayDate = currentDate.getDate();
+document.querySelector(".todayDate").innerText = fullDate
+document.querySelector()
+
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+
+
