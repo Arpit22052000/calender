a2127274281b43b7b24bfe112526c034ed676e81
Arpit22052000 <a.kabra470@gmail.com>
2023-12-13 19:52:08 +0530

    function-create

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
-const currentDate = new Date();
-const fullDate = currentDate.toDateString();
+let currentDate = new Date; // get current date
+currentDate.setMonth(11);
+let first = currentDate.getDate() - currentDate.getDay(); // week first day, sunday
+console.log("currentDate", currentDate)
+console.log("currentDate.getDate()", currentDate.getDate())
+console.log("currentDate.getDay()", currentDate.getDay())
+
+//top type of date showcase
+let fullDate = currentDate.toDateString();
+let fullDate1 = currentDate.toLocaleString();
+document.querySelector(".dayFormat").innerText = fullDate;
+
+// evenlistener on fullDate
+const change = document.querySelector('.myBtn');
+change.addEventListener("click", changeFormat);
+function changeFormat() {
+    document.querySelector(".dayFormat").innerText = fullDate1;
+}
 
 
 
 
-// const type = document.querySelector(".dateType")
-// type.addEventListener("click", () => {
-//     dayFormat()
-// })
+// Days under week
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+
+for (let i = 0; i < days.length; i++) {
+    const day = days[i];
+    const index = i + first;
+    document.querySelector(`.${day}`).innerText = index;
+}
 
 
-// function dayFormat() {
-//     alert("Choose the day format :")
-//     const fullDate = currentDate.toDateString();
-//     const localeDate = currentDate.toLocaleDateString();
-//     const localeTime = currentDate.toLocaleTimeString();
-//     const localeString = currentDate.toLocaleString();
+// button previous 
+const prev = document.querySelector('.previous');
+prev.addEventListener("click", previous);
+function previous() {
+    first = first - 7;
+    for (let i = 0; i < days.length; i++) {
+        const day = days[i];
+        const index = i + first;
+        document.querySelector(`.${day}`).innerText = index;
+    }
+}
 
-// }
-
-
-
-
-
-
-console.log(fullDate)
-console.log(localeDate)
-console.log(localeTime)
-console.log(localeString)
-
-
-const todayDate = currentDate.getDate();
-document.querySelector(".todayDate").innerText = fullDate
-document.querySelector()
-
-const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// button next
+const next = document.querySelector('.next');
+next.addEventListener("click", nextDay);
+function nextDay() {
+    first = first + 7;
+    for (let i = 0; i < days.length; i++) {
+        const day = days[i];
+        const index = i + first;
+        document.querySelector(`.${day}`).innerText = index;
+    }
+}
 
 
 
